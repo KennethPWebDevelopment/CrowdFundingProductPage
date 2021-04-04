b286a76c3f1fb07ee1245e84a5020012dea49715
Kenneth Paige <kpwebdevelopment@gmail.com>
2021-04-04 07:57:22 -0400

    Add functionality to buttons to decrease select reward amount

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 
+// SELECT REWARD BUTTON 1
 
-$("p").click(function(){
-    alert("The paragraph was clicked.");
-  });
+// TARGET SELECT REWARD BUTTON
 
-  $("#bookmark-btn").click(function() {
-      alert("clicked");
-  })
+var amountLeftButton = $(".amountLeftButton1");
+
+// // DECIDE WHAT WILL HAPPEN ON CLICK
+
+    amountLeftButton.click(function() {
+        var amount = $(".numbers101").html();
+
+        var number = parseInt(amount); 
+
+        number --; 
+
+        $(".numbers101").text(number);
+        ;
+      });
+
+// SELECT REWARD BUTTON 2      
+
+// TARGET SELECT REWARD BUTTON
+
+
+      var amountLeftButton2 = $(".amountLeftButton2");
+
+// // DECIDE WHAT WILL HAPPEN ON CLICK
+
+    amountLeftButton2.click(function() {
+        var amount = $(".numbers64").html();
+
+        var number = parseInt(amount); 
+
+        number --; 
+
+        $(".numbers64").text(number);
+        ;
+      });
